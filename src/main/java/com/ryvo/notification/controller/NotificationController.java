d6e0982642ab67322c9e81c481d9b12ea16fbaea
cayna <cayna@agapesistemas.com.br>
2025-03-21 02:01:37 -0300

    initial commit

--- src/main/java/com/ryvo/notification/controller/NotificationController.java
+++ src/main/java/com/ryvo/notification/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package com.ryvo.notification.controller;
 import com.ryvo.notification.config.MessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class NotificationController {
     public String sendMessage(@RequestBody String message) {
         messageHandler.sendMessageToAll(message);
         return "Mensagem enviada para todos os clientes WebSocket!";
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World!";
     }
 
 }
