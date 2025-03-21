cad90a421f545c2205469fb363b61ed255ca02ce
cayna <cayna@agapesistemas.com.br>
2025-03-21 01:39:55 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# First stage: Build the application
+FROM openjdk:17-jdk-slim as build
+
+RUN apt-get update && apt-get install -y maven
+
+COPY src /app/src
+COPY pom.xml /app
+
+WORKDIR /app
+
+RUN mvn -Dmaven.test.skip=true package
+
+# Second stage: Create the runtime image
+FROM openjdk:17-jdk-slim
+WORKDIR /app
+
+# Copy the jar file from the build stage
+COPY --from=build /app/target/*.jar /app/app.jar
+
+EXPOSE 1234
+
+# Default command to run the jar file
+ENTRYPOINT ["sh", "-c", "exec java ${JAVA_OPTS} -jar /app/app.jar"]
---
+++ src/main/java/com/ryvo/notification/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.ryvo.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import com.ryvo.notification.config.MessageHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MessageHandler(), "/ws").setAllowedOrigins("*");
+    }
+}
